2827293ea84049970df44fb188b3bd4d37d93a87
Eric K. Sender <esender@gatech.edu>
2015-06-25 10:43:49 -0700

    Lesson1

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.android.sunshine.app;
 
+import android.os.NetworkOnMainThreadException;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -50,6 +65,8 @@ public class MainActivity extends ActionBarActivity {
      */
     public static class PlaceholderFragment extends Fragment {
 
+        ArrayAdapter<String> mForecastAdapter;
+
         public PlaceholderFragment() {
         }
 
@@ -57,7 +74,58 @@ public class MainActivity extends ActionBarActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            String apiString = streamToString();
+
+            ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+            listView.setAdapter(mForecastAdapter);
+
             return rootView;
+        }
+
+        private String streamToString()  {
+            HttpURLConnection urlConnection = null;
+            URL url = null;
+            try {
+                url = new URL("http://api.openweathermap.org/data/2.5/forecast/daily?q=94043,us&mode=json&units=metric&cnt=7");
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+                return null;
+            }
+            StringBuilder sb = new StringBuilder();
+            try {
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                InputStream inputStream = urlConnection.getInputStream();
+                if (inputStream == null) return null;
+                BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+                // try (BufferedReader reader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()))) {
+                try {
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        sb.append(line + '\n');
+                    }
+                } finally {
+                    if (reader != null) reader.close();
+                }
+
+            } catch(NetworkOnMainThreadException e) {
+                Log.e(getClass().getName(), "Error ", e);
+                return null;
+            } catch (IOException e) {
+                Log.e("PlaceholderFragment", "Error ", e);
+                // If the code didn't successfully get the weather data, there's no point in attemping
+                // to parse it.
+                return null;
+            } finally {
+                if (urlConnection != null)
+                    urlConnection.disconnect();
+            }
+
+
+            return sb.length() == 0 ? null : sb.toString();
         }
     }
 }
